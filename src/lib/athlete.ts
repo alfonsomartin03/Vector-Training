f48fee4b3e16a7fea891849d6272e092422ee991
Alfonso Martin <alfonsomartin@Alfonsos-MacBook-Pro.local>
2026-09-18 22:42:57 -0400

    rebuild home page to show current training

--- src/lib/athlete.ts
+++ src/lib/athlete.ts
@@ -1,9 +1,9 @@
 import type {
     AthleteData,
-  AthleteProfile,
-  LactateTest,
-  PowerProfile,
-  Vo2MaxTest,
+    AthleteProfile,
+    LactateTest,
+    PowerProfile,
+    Vo2MaxTest,
 } from "../types/athlete";
 
 import { supabase } from "./supabase";
